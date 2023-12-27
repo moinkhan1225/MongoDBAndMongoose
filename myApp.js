60692c837f6abab6d9ba1b7ed17aa34aa42abe17
Hakai <moinkhanrobin1@gmail.com>
2023-12-27 15:58:23 +0530

    Query Chain --FIX 2

--- myApp.js
+++ myApp.js
@@ -105,11 +105,15 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  Person.findOneByFood({favoriteFoods:[foodToSearch]},((err,data)=>{
-    if(err) return console.error(err)
-      const UpdateData = data.sort({name:1}).limit(2).select({age:0})
-    done(null , UpdateData);
-  }))
+  Person.find(
+    {favoriteFoods:[foodToSearch]}
+    ).sort({name:1})
+    .limit(2)
+    .select({age:0})
+    .exec((err,data)=>{
+      if(err) return console.error(err)
+      done(null,data)
+    })
 };
 
 /** **Well Done !!**
